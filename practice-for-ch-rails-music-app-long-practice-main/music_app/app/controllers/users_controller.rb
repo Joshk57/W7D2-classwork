691410531cbe6c4b4b9fb7f1d19c5225cce7787f
joshua <joshk.5793@gmail.com>
2023-04-18 14:38:59 -0400

    fixed issues

---
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+    before_action :require_logged_out
+
+    def index
+        @users = User.all
+        render :index
+    end
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render :show
+    end
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            redirect_to 
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+end
---
+++ practice-for-ch-rails-music-app-long-practice-main/music_app/app/models/user.rb
@@ -0,0 +1,50 @@
+class User < ApplicationRecord
+    validates :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+
+    before_validation :ensure_session_token
+
+    attr_reader :password
+    
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    private
+
+    def generate_unique_session_token
+
+        loop do
+            session_token = SecureRandom::urlsafe_base64(16)
+            return session_token unless User.exists?(session_token: session_token)
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+
+    end
+end
